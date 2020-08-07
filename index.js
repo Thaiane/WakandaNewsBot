8deccbf09bea15df90c01f0b169c6b80d9f218c6
Thaiane_Braga@gap.com <Thaiane_Braga@gap.com>
2020-08-06 22:30:24 -0300

    remove logs

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 var Twit = require('twit');
 var Twitter = new Twit(require('./config.js'));
 
-
 function postHello() {
-    console.log("Oi, deu bom")
     Twitter.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
         console.log(data)
-        console.log("Oi, deu ruim")
     });
 }
-
 
 function main() {
     postHello();
