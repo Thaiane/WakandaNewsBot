93d732cd5c0465b15a699ece466dc925b7238e2f
Thaiane_Braga@gap.com <Thaiane_Braga@gap.com>
2020-08-07 23:16:49 -0300

    Add other function about post

--- index.js
+++ index.js
@@ -7,6 +7,31 @@ function postHello() {
     });
 }
 
+const stream = T.stream('statuses/filter', {track: 'Wakanda'});
+
+stream.on('tweet', tweet => {
+    console.log("Achei")
+});
+
+Twitter.get(
+    'search/tweets',
+    { q: getSearchKey(), count: 50, lang: 'en' },
+    function(err, data, response) {
+        let tweetList = [];
+        if (!err) {
+            for (let i = 0; i < data.statuses.length; i++) {
+                let tweet = data.statuses[i];
+                tweetList.push(tweet);
+            }
+            callback(tweetList);
+            return;
+        } else {
+            console.log(err);
+        }
+    }
+);
+}
+
 function main() {
     postHello();
 }
